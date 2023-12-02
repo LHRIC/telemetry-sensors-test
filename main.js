12ca989f13df7d9b4fc68606f343e3360276e511
gracem77 <gracem77.hp@gmail.com>
2023-12-02 14:40:26 -0600

    frontend for telemetry app courtesy g

--- main.js
+++ main.js
@@ -3,16 +3,20 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
-    })
+      height: 600,
+      backgroundColor: '#61dafbaa',
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
   
-    win.loadFile('index.html')
+    win.loadFile('index.html');
   }
 
 app.whenReady().then(() => {
 createWindow()
-})
+});
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+    if (process.platform !== 'darwin') app.quit();
+  });
