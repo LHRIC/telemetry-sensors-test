9603bff559c3e4794e9c473efc53b4acd50bad4e
gracem77 <gracem77.hp@gmail.com>
2023-12-02 11:41:21 -0600

    electron setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+  win.loadFile('index.html')
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
